67c34e23109dabb0d5ec1fa08cdfadb608e598e5
Qi-Month <dy757410@163.com>
2026-06-06 21:38:41 +0800

    lib

--- src/main/java/dev/celestiacraft/star_chaser/common/tool/SCToolTiers.java
+++ src/main/java/dev/celestiacraft/star_chaser/common/tool/SCToolTiers.java
@@ -1,37 +1,27 @@
 package dev.celestiacraft.star_chaser.common.tool;
 
+import dev.celestiacraft.libs.api.register.tool.tier.TierBuilder;
 import dev.celestiacraft.star_chaser.StarChaser;
 import dev.celestiacraft.star_chaser.tag.SCBlockTag;
 import dev.celestiacraft.star_chaser.tag.SCItemTag;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.ForgeTier;
-import net.minecraftforge.common.TierSortingRegistry;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-import java.util.function.Supplier;
 
 public class SCToolTiers {
 	public static final Tier FLINT;
 
 	static {
-		FLINT = TierSortingRegistry.registerTier(
-				new ForgeTier(
-						1,
-						64,
-						2.5f,
-						0.5f,
-						15,
-						SCBlockTag.NEED_FLINT_TOOLS,
-						() -> Ingredient.of(SCItemTag.FLINTS)
-				),
-				StarChaser.loadResource("flint"),
-				List.of(Tiers.WOOD),
-				List.of(Tiers.STONE)
-		);
+		FLINT = TierBuilder.create(builder -> {
+			builder.setName(StarChaser.loadResource("flint"))
+					.setLevel(1)
+					.setUses(64)
+					.setSpeed(2.5f)
+					.setDamage(0.5f)
+					.setEnchantmentValue(15)
+					.setMineableTag(SCBlockTag.NEED_FLINT_TOOLS)
+					.setRepairIngredient(SCItemTag.FLINTS)
+					.setAfter(Tiers.WOOD)
+					.setBefore(Tiers.STONE);
+		});
 	}
 }
